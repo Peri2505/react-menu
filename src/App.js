dc42edade02aa1f1a46db2f6275a11e2148cb47c
Peri2505 <moldobaevaperi@yahoo.com>
2023-12-20 15:49:56 -0600

    react-menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
 
-function App() {
+// const tempCategpries = menu.map((item) => item.category);
+// //console.log(tempCategpries);
+// const tempSet = new Set(tempCategpries);
+// //console.log(tempSet);
+// const tempItems = ["all", ...tempSet];
+// //console.log(tempItems);
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+console.log(allCategories);
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
-}
-
+};
 export default App;
